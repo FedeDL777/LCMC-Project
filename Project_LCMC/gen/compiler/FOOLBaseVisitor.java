860123a30e4f5b467c64114e1a7686a0f2a77451
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2026-02-22 20:25:56 +0100

    Refactor del progetto con antlr settato giusto

---
+++ Project_LCMC/gen/compiler/FOOLBaseVisitor.java
@@ -0,0 +1,190 @@
+// Generated from C:/Users/feded/OneDrive/UniMagistrale/PrimoAnno/PrimoSemestre/LinguaggiECompilatori/Progetto LCMC/LCMC-Project/Project_LCMC/src/compiler/FOOL.g4 by ANTLR 4.13.2
+package compiler;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link FOOLVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class FOOLBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements FOOLVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProg(FOOLParser.ProgContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitLetInProg(FOOLParser.LetInProgContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNoDecProg(FOOLParser.NoDecProgContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVardec(FOOLParser.VardecContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFundec(FOOLParser.FundecContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMinus(FOOLParser.MinusContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMin_eq(FOOLParser.Min_eqContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOr(FOOLParser.OrContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPars(FOOLParser.ParsContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFalse(FOOLParser.FalseContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitInteger(FOOLParser.IntegerContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitEq(FOOLParser.EqContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPlus(FOOLParser.PlusContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCall(FOOLParser.CallContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDiv(FOOLParser.DivContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNot(FOOLParser.NotContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrint(FOOLParser.PrintContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTimes(FOOLParser.TimesContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAnd(FOOLParser.AndContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTrue(FOOLParser.TrueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitId(FOOLParser.IdContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIf(FOOLParser.IfContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMag_eq(FOOLParser.Mag_eqContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIntType(FOOLParser.IntTypeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitBoolType(FOOLParser.BoolTypeContext ctx) { return visitChildren(ctx); }
+}
---
+++ Project_LCMC/src/compiler/FOOL.g4
@@ -0,0 +1,96 @@
+grammar FOOL;
+ 
+@lexer::members {
+public int lexicalErrors=0;
+}
+   
+/*------------------------------------------------------------------
+ * PARSER RULES
+ *------------------------------------------------------------------*/
+
+prog  : progbody EOF ;
+     
+progbody : LET dec+ IN exp SEMIC  #letInProg
+         | exp SEMIC              #noDecProg
+         ;
+ 
+dec : VAR ID COLON type ASS exp SEMIC  #vardec
+    | FUN ID COLON type LPAR (ID COLON type (COMMA ID COLON type)* )? RPAR 
+        	(LET dec+ IN)? exp SEMIC   #fundec
+    ;
+        //parser use the top-down priority, meaning that the top expressions
+        //have greater priority, ex: TIMES > PLUS > PRINT
+exp     :NOT exp #not
+        | exp TIMES exp #times
+        | exp DIV exp   #div
+        | exp PLUS  exp #plus
+        | exp MINUS exp #minus
+        | exp EQ  exp   #eq
+        | exp MIN_EQ  exp   #min_eq
+        | exp MAG_EQ  exp   #mag_eq
+        | exp AND exp #and
+        | exp OR exp #or
+
+        | LPAR exp RPAR #pars
+    	| MINUS? NUM #integer
+	    | TRUE #true     
+	    | FALSE #false
+	    | IF exp THEN CLPAR exp CRPAR ELSE CLPAR exp CRPAR  #if   
+	    | PRINT LPAR exp RPAR #print
+	    | ID #id
+	    | ID LPAR (exp (COMMA exp)* )? RPAR #call
+        ; 
+             
+type    : INT #intType
+        | BOOL #boolType
+ 	    ;  
+ 	  		  
+/*------------------------------------------------------------------
+ * LEXER RULES
+ *------------------------------------------------------------------*/
+
+PLUS  	: '+' ;
+MINUS	: '-' ; 
+TIMES   : '*' ;
+//New division
+DIV     : '/';
+LPAR	: '(' ;
+RPAR	: ')' ;
+CLPAR	: '{' ;
+CRPAR	: '}' ;
+SEMIC 	: ';' ;
+COLON   : ':' ; 
+COMMA	: ',' ;
+EQ	    : '==' ;	
+ASS	    : '=' ;
+//New comparators
+MIN_EQ  : '<=' ;
+MAG_EQ  : '>=' ;
+NOT  : '!' ;
+//And\OR
+AND     : '&&' ;
+OR      : '||' ;
+TRUE	: 'true' ;
+FALSE	: 'false' ;
+IF	    : 'if' ;
+THEN	: 'then';
+ELSE	: 'else' ;
+PRINT	: 'print' ;
+LET     : 'let' ;	
+IN      : 'in' ;	
+VAR     : 'var' ;
+FUN	    : 'fun' ;	  
+INT	    : 'int' ;
+BOOL	: 'bool' ;
+NUM     : '0' | ('1'..'9')('0'..'9')* ; 
+
+ID  	: ('a'..'z'|'A'..'Z')('a'..'z' | 'A'..'Z' | '0'..'9')* ;
+
+
+WHITESP  : ( '\t' | ' ' | '\r' | '\n' )+    -> channel(HIDDEN) ;
+
+COMMENT : '/*' .*? '*/' -> channel(HIDDEN) ;
+ 
+ERR   	 : . { System.out.println("Invalid char "+getText()+" at line "+getLine()); lexicalErrors++; } -> channel(HIDDEN); 
+
+
