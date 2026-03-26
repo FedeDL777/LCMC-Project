cbaf7665a6f8358bd6181e38b8d31aa44f0c6399
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-03-26 22:51:30 +0100

    fix: updated CodeGenerationASTVisitor with professor code

--- src/compiler/CodeGenerationASTVisitor.java
+++ src/compiler/CodeGenerationASTVisitor.java
@@ -7,8 +7,8 @@ import static compiler.lib.FOOLlib.*;
 
 public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidException> {
 
-  CodeGenerationASTVisitor() {}
-  CodeGenerationASTVisitor(boolean debug) {super(false,debug);} //enables print for debugging
+	CodeGenerationASTVisitor() {}
+	CodeGenerationASTVisitor(boolean debug) {super(false,debug);} //enables print for debugging
 
 	@Override
 	public String visitNode(ProgLetInNode n) {
@@ -16,11 +16,11 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		String declCode = null;
 		for (Node dec : n.declist) declCode=nlJoin(declCode,visit(dec));
 		return nlJoin(
-			"push 0",	
-			declCode, // generate code for declarations (allocation)			
-			visit(n.exp),
-			"halt",
-			getCode()
+				"push 0",
+				declCode, // generate code for declarations (allocation)
+				visit(n.exp),
+				"halt",
+				getCode()
 		);
 	}
 
@@ -28,30 +28,40 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(ProgNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.exp),
-			"halt"
+				visit(n.exp),
+				"halt"
 		);
 	}
-	@Override
-	public String visitNode(ClassNode n) {
-	  return "";
-	}
-
 
 	@Override
 	public String visitNode(FunNode n) {
 		if (print) printNode(n,n.id);
-		for (ParNode par : n.parlist) visit(par);
-		for (Node dec : n.declist) visit(dec);
-		visit(n.exp);
+		String declCode = null, popDecl = null, popParl = null;
+		for (Node dec : n.declist) {
+			declCode = nlJoin(declCode,visit(dec));
+			popDecl = nlJoin(popDecl,"pop");
+		}
+		for (int i=0;i<n.parlist.size();i++) popParl = nlJoin(popParl,"pop");
 		String funl = freshFunLabel();
 		putCode(
-			nlJoin(
-				funl+":"
-				//codice del corpo della funzione
-			)
+				nlJoin(
+						funl+":",
+						"cfp", // set $fp to $sp value
+						"lra", // load $ra value
+						declCode, // generate code for local declarations (they use the new $fp!!!)
+						visit(n.exp), // generate code for function body expression
+						"stm", // set $tm to popped value (function result)
+						popDecl, // remove local declarations from stack
+						"sra", // set $ra to popped value
+						"pop", // remove Access Link from stack
+						popParl, // remove parameters from stack
+						"sfp", // set $fp to popped value (Control Link)
+						"ltm", // load $tm value (function result)
+						"lra", // load $ra value
+						"js"  // jump to to popped address
+				)
 		);
-		return "push "+funl;		
+		return "push "+funl;
 	}
 
 	@Override
@@ -64,42 +74,42 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(PrintNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.exp),
-			"print"
+				visit(n.exp),
+				"print"
 		);
 	}
 
 	@Override
 	public String visitNode(IfNode n) {
 		if (print) printNode(n);
-	 	String l1 = freshLabel();
-	 	String l2 = freshLabel();		
+		String l1 = freshLabel();
+		String l2 = freshLabel();
 		return nlJoin(
-			visit(n.cond),
-			"push 1",
-			"beq "+l1,
-			visit(n.el),
-			"b "+l2,
-			l1+":",
-			visit(n.th),
-			l2+":"
+				visit(n.cond),
+				"push 1",
+				"beq "+l1,
+				visit(n.el),
+				"b "+l2,
+				l1+":",
+				visit(n.th),
+				l2+":"
 		);
 	}
 
 	@Override
 	public String visitNode(EqualNode n) {
 		if (print) printNode(n);
-	 	String l1 = freshLabel();
-	 	String l2 = freshLabel();
+		String l1 = freshLabel();
+		String l2 = freshLabel();
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"beq "+l1,
-			"push 0",
-			"b "+l2,
-			l1+":",
-			"push 1",
-			l2+":"
+				visit(n.left),
+				visit(n.right),
+				"beq "+l1,
+				"push 0",
+				"b "+l2,
+				l1+":",
+				"push 1",
+				l2+":"
 		);
 	}
 
@@ -107,129 +117,19 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(TimesNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"mult"
-		);	
+				visit(n.left),
+				visit(n.right),
+				"mult"
+		);
 	}
 
 	@Override
 	public String visitNode(PlusNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"add"				
-		);
-	}
-
-	@Override
-	public String visitNode(NotNode n) {
-		if (print) printNode(n);
-		// !exp: if exp==true then false else true
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
-			visit(n.exp),
-			"push 1",
-			"beq "+l1,
-			"push 1",   // was false (0), negated becomes true (1)
-			"b "+l2,
-			l1+":",
-			"push 0",   // was true (1), negated becomes false (0)
-			l2+":"
-		);
-	}
-
-	@Override
-	public String visitNode(MinusNode n) {
-		if (print) printNode(n);
-		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"sub"
-		);
-	}
-
-	@Override
-	public String visitNode(DivNode n) {
-		if (print) printNode(n);
-		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"div"
-		);
-	}
-
-	@Override
-	public String visitNode(LessEqualNode n) {
-		if (print) printNode(n);
-		// a <= b: bleq jumps if second popped <= first popped, i.e. a <= b
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"bleq "+l1,
-			"push 0",
-			"b "+l2,
-			l1+":",
-			"push 1",
-			l2+":"
-		);
-	}
-
-	@Override
-	public String visitNode(GreaterEqualNode n) {
-		if (print) printNode(n);
-		// a >= b is equivalent to b <= a (swap operands)
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
-			visit(n.right),  // push b first
-			visit(n.left),   // push a second
-			"bleq "+l1,      // jumps if b <= a, i.e. a >= b
-			"push 0",
-			"b "+l2,
-			l1+":",
-			"push 1",
-			l2+":"
-		);
-	}
-
-	@Override
-	public String visitNode(AndNode n) {
-		if (print) printNode(n);
-		// a && b: if a is false (0), result is false; otherwise result is b (short-circuit)
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
-			visit(n.left),
-			"push 0",
-			"beq "+l1,     // if left == 0, short-circuit to false
-			visit(n.right),
-			"b "+l2,
-			l1+":",
-			"push 0",
-			l2+":"
-		);
-	}
-
-	@Override
-	public String visitNode(OrNode n) {
-		if (print) printNode(n);
-		// a || b: if a is true (1), result is true; otherwise result is b (short-circuit)
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
-			visit(n.left),
-			"push 1",
-			"beq "+l1,     // if left == 1, short-circuit to true
-			visit(n.right),
-			"b "+l2,
-			l1+":",
-			"push 1",
-			l2+":"
+				visit(n.left),
+				visit(n.right),
+				"add"
 		);
 	}
 
@@ -240,16 +140,16 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		for (int i=n.arglist.size()-1;i>=0;i--) argCode=nlJoin(argCode,visit(n.arglist.get(i)));
 		for (int i = 0;i<n.nl-n.entry.nl;i++) getAR=nlJoin(getAR,"lw");
 		return nlJoin(
-			"lfp", // load Control Link (pointer to frame of function "id" caller)
-			argCode, // generate code for argument expressions in reversed order
-			"lfp", getAR, // retrieve address of frame containing "id" declaration
-                          // by following the static chain (of Access Links)
-            "stm", // set $tm to popped value (with the aim of duplicating top of stack)
-            "ltm", // load Access Link (pointer to frame of function "id" declaration)
-            "ltm", // duplicate top of stack
-            "push "+n.entry.offset, "add", // compute address of "id" declaration
-			"lw", // load address of "id" function
-            "js"  // jump to popped address (saving address of subsequent instruction in $ra)
+				"lfp", // load Control Link (pointer to frame of function "id" caller)
+				argCode, // generate code for argument expressions in reversed order
+				"lfp", getAR, // retrieve address of frame containing "id" declaration
+				// by following the static chain (of Access Links)
+				"stm", // set $tm to popped value (with the aim of duplicating top of stack)
+				"ltm", // load Access Link (pointer to frame of function "id" declaration)
+				"ltm", // duplicate top of stack
+				"push "+n.entry.offset, "add", // compute address of "id" declaration
+				"lw", // load address of "id" function
+				"js"  // jump to popped address (saving address of subsequent instruction in $ra)
 		);
 	}
 
@@ -259,10 +159,10 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		String getAR = null;
 		for (int i = 0;i<n.nl-n.entry.nl;i++) getAR=nlJoin(getAR,"lw");
 		return nlJoin(
-			"lfp", getAR, // retrieve address of frame containing "id" declaration
-			              // by following the static chain (of Access Links)
-			"push "+n.entry.offset, "add", // compute address of "id" declaration
-			"lw" // load value of "id" variable
+				"lfp", getAR, // retrieve address of frame containing "id" declaration
+				// by following the static chain (of Access Links)
+				"push "+n.entry.offset, "add", // compute address of "id" declaration
+				"lw" // load value of "id" variable
 		);
 	}
 
